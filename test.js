339378f669223c4c3ed4547855fd78b79db38d28
Jérôme Desboeufs <jerome@livingdata.co>
2023-03-27 14:59:46 +0200

    Use Workers and SharedArrayBuffer

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-import {search, closeDb} from './lib/search.js'
+
+import {Worker} from 'node:worker_threads'
+import {pEvent} from 'p-event'
+import PQueue from 'p-queue'
+import readBigFile from './lib/read-big-file.js'
+
+const rtreeBuffer = await readBigFile('./data/parcelles.lmdb-rtree')
 
 const minLat = 42.23064
 const maxLat = 50.69999
@@ -12,13 +18,79 @@ function getRandomLocation() {
   return [lon, lat]
 }
 
-console.time('lookup 100k items')
 
-for (let i = 0; i < 100_000; i++) {
-  const location = getRandomLocation()
-  await search(location)
+
+async function createWorker() {
+  const worker = new Worker('./lib/worker.js')
+  worker.postMessage({event: 'configure', config: {rtreeBuffer}})
+  const response = await pEvent(worker, 'message')
+
+  if (response.error) {
+    throw new Error(response.error)
+  }
+
+  return worker
 }
 
-console.timeEnd('lookup 100k items')
+const _workers = [
+  await createWorker(),
+  await createWorker(),
+  await createWorker(),
+  await createWorker()
+]
 
-await closeDb()
+const _jobsQueue = []
+const _idleWorkers = [..._workers]
+
+function enqueueJob(job) {
+  return new Promise((resolve, reject) => {
+    _jobsQueue.push({job, resolve, reject})
+    execNextJob()
+  })
+}
+
+async function execNextJob() {
+  if (_jobsQueue.length === 0) {
+    return
+  }
+
+  if (_idleWorkers.length === 0) {
+    return
+  }
+
+  const job = _jobsQueue.shift()
+  const worker = _idleWorkers.shift()
+
+  worker.postMessage({event: 'job', job: job.job})
+  const result = await pEvent(worker, 'message')
+
+  _idleWorkers.push(worker)
+
+  if (result.error) {
+    job.reject(new Error(result.error))
+  } else {
+    job.resolve(result.result)
+  }
+
+  execNextJob()
+}
+
+console.time('lookup 1m items')
+
+const q = new PQueue({concurrency: 4})
+
+for (let i = 0; i < 1_000_000; i++) {
+  const location = getRandomLocation()
+  q.add(() => enqueueJob({location}))
+  await q.onSizeLessThan(16)
+}
+
+await q.onIdle()
+
+console.timeEnd('lookup 1m items')
+
+await Promise.all(_workers.map(async worker => {
+  worker.postMessage({event: 'close'})
+  await pEvent(worker, 'message')
+  worker.terminate()
+}))
